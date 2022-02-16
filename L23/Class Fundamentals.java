2b839498ee4ac932bea116a3bf68799f6f741e87
Saurav Kumar <61316762+sauravhathi@users.noreply.github.com>
2022-02-16 13:38:30 +0530

    first commit

---
+++ L23/Class Fundamentals.java
@@ -0,0 +1,20 @@
+Q3
+
+package q11155;
+public class Student {
+	private String id;
+	private String name;
+	private int age;
+	private char gender;
+}
+
+
+Q4
+
+package q11217;
+public class Student {
+	private String id;
+	private String name;
+	private int age;
+	private char gender;
+}
